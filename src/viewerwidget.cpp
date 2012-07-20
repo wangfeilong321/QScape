749240da9b90d851c49c89790be3d1a0283cfdba
Christian Wygoda <arsgeografica@gmail.com>
2012-07-20 11:59:24 +0200

    Viewer cleanup

--- src/viewerwidget.cpp
+++ src/viewerwidget.cpp
@@ -25,11 +25,13 @@ ViewerWidget::ViewerWidget(osgViewer::ViewerBase::ThreadingModel threadingModel,
 QWidget* ViewerWidget::createWidget(osg::Camera* camera, osg::Node* scene)
 {
     setCamera(camera);
+    setCameraManipulator(new osgGA::TrackballManipulator());
 
     setSceneData(scene);
-    addEventHandler( new osgViewer::StatsHandler );
-    setCameraManipulator( new osgGA::TrackballManipulator() );
-    osgQt::GraphicsWindowQt* gw = dynamic_cast<osgQt::GraphicsWindowQt*>( camera->getGraphicsContext() );
+
+    addEventHandler(new osgViewer::StatsHandler);
+
+    osgQt::GraphicsWindowQt* gw = dynamic_cast<osgQt::GraphicsWindowQt*>(camera->getGraphicsContext());
 
     return gw->getGLWidget();
 }
@@ -38,7 +40,7 @@ osg::Camera* ViewerWidget::createCamera(int x, int y, int w, int h, QString name
 {
     osg::DisplaySettings* ds = osg::DisplaySettings::instance().get();
     osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
-    //traits->windowName = "";
+    traits->windowName = name.toAscii().data();
     traits->windowDecoration = false;
     traits->x = 0;
     traits->y = 0;
@@ -53,7 +55,7 @@ osg::Camera* ViewerWidget::createCamera(int x, int y, int w, int h, QString name
     osg::ref_ptr<osg::Camera> camera = new osg::Camera;
 
     camera->setGraphicsContext( new osgQt::GraphicsWindowQt(traits.get()) );
-    camera->setClearColor( osg::Vec4(0.18, 0.18, 0.18, 0.0) );
+    camera->setClearColor( osg::Vec4(0.82, 0.82, 0.82, 0.0) );
     camera->setViewport( new osg::Viewport(0, 0, traits->width, traits->height) );
     camera->setProjectionMatrixAsPerspective(
         30.0f, static_cast<double>(traits->width)/static_cast<double>(traits->height), 1.0f, 10000.0f );
